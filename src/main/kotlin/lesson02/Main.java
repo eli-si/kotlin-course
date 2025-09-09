7c296cd7018d915943ff7c82574acf33b8b9b41b
Ilya Silich <eliassi4th@gmail.com>
2025-09-09 15:58:25 -0400

    create new branch

---
+++ src/main/kotlin/lesson02/Main.java
@@ -0,0 +1,14 @@
+package lesson02;
+
+import static java.sql.DriverManager.println;
+
+public class Main {
+    fun main() {
+        val message = "I Love Kotlin";
+        println(message);
+        return null;
+    }
+
+    private void println(val message) {
+    }
+}
